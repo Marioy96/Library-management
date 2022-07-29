36854da49d512fa807e3997de7618b45df9fb4e5
carlocasiglia <carlo.casiglia@develhope.co>
2022-07-29 11:40:17 +0200

    project reinitialized with correct project structure and correct main branch

---
+++ src/test/java/co/develhope/librarymanagement/LibraryManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package co.develhope.librarymanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
