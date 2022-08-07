49f6c53e445cf1b8cff43a3deed4f03e3744ab5c
Giuseppe Roberti <giuseppe.roberti@informationline.it>
2022-08-07 16:19:42 +0200

    Aggiunti set/get e costruttori nelle classi Employee e Customer, e aggiunte le anntoazioni per JPA e la creazione del database,aggiunti i sevice e i repository

---
+++ src/main/java/co/develhope/librarymanagement/service/CustomerService.java
@@ -0,0 +1,9 @@
+package co.develhope.librarymanagement.service;
+
+import org.springframework.stereotype.Service;
+
+
+// TODO implementare i metodi crud nel service,per gli esempi guarda la classe AuthorService
+@Service
+public class CustomerService {
+}
---
+++ src/main/java/co/develhope/librarymanagement/service/EmployeeService.java
@@ -0,0 +1,8 @@
+package co.develhope.librarymanagement.service;
+
+import org.springframework.stereotype.Service;
+
+// TODO implementare i metodi crud nel service,per gli esempi guarda la classe AuthorService
+@Service
+public class EmployeeService {
+}
