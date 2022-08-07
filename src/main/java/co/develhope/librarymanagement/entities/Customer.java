26927eba2067325007ec18bc279005d25bc00e84
Giuseppe Roberti <giuseppe.roberti@informationline.it>
2022-08-07 16:14:04 +0200

    Aggiunti set/get e costruttori nelle classi Employee e Customer, e aggiunte le anntoazioni per JPA e la creazione del database

--- src/main/java/co/develhope/librarymanagement/entities/Customer.java
+++ src/main/java/co/develhope/librarymanagement/entities/Customer.java
@@ -1,14 +1,90 @@
 package co.develhope.librarymanagement.entities;
 
-// TODO Entity and Table annotations
-// TODO Id, constructors, getters and setters
 
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "customer")
 public class Customer {
-
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id", nullable = false)
+    private Long id;
     private String name;
     private String surname;
     private String telephoneNumber;
+    private String email;
+    private Boolean turnAvailability;
+    private LocalDate absences;
+
+    public Customer(String name, String surname, String telephoneNumber, String email, Boolean turnAvailability, LocalDate absences) {
+        this.name = name;
+        this.surname = surname;
+        this.telephoneNumber = telephoneNumber;
+        this.email = email;
+        this.turnAvailability = turnAvailability;
+        this.absences = absences;
+    }
+
+    public Customer() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getTelephoneNumber() {
+        return telephoneNumber;
+    }
+
+    public void setTelephoneNumber(String telephoneNumber) {
+        this.telephoneNumber = telephoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Boolean getTurnAvailability() {
+        return turnAvailability;
+    }
+
+    public void setTurnAvailability(Boolean turnAvailability) {
+        this.turnAvailability = turnAvailability;
+    }
+
+    public LocalDate getAbsences() {
+        return absences;
+    }
+
+    public void setAbsences(LocalDate absences) {
+        this.absences = absences;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 
 }
 
--- src/main/java/co/develhope/librarymanagement/entities/Employee.java
+++ src/main/java/co/develhope/librarymanagement/entities/Employee.java
@@ -1,11 +1,17 @@
 package co.develhope.librarymanagement.entities;
 
+import javax.persistence.*;
 import java.time.LocalDate;
 
-// TODO Entity and Table annotations
-// TODO Id, constructors, getters and setters
 
+
+@Entity
+@Table
 public class Employee {
+    @Id
+    @Column(name = "id", nullable = false)
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
 
     private String name;
     private String surname;
@@ -13,5 +19,89 @@ public class Employee {
     private String telephoneNumber;
     private LocalDate dateOfBirth;
     private LocalDate dateOfHiring;
+    @Column(unique = true)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private String employeeCode;
+
+    public Employee() {
+    }
+
+    public Employee(Long id, String name, String surname, String address, String telephoneNumber, LocalDate dateOfBirth, LocalDate dateOfHiring, String employeeCode) {
+        this.id = id;
+        this.name = name;
+        this.surname = surname;
+        this.address = address;
+        this.telephoneNumber = telephoneNumber;
+        this.dateOfBirth = dateOfBirth;
+        this.dateOfHiring = dateOfHiring;
+        this.employeeCode = employeeCode;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getTelephoneNumber() {
+        return telephoneNumber;
+    }
+
+    public void setTelephoneNumber(String telephoneNumber) {
+        this.telephoneNumber = telephoneNumber;
+    }
+
+    public LocalDate getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(LocalDate dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public LocalDate getDateOfHiring() {
+        return dateOfHiring;
+    }
+
+    public void setDateOfHiring(LocalDate dateOfHiring) {
+        this.dateOfHiring = dateOfHiring;
+    }
+
+    public String getEmployeeCode() {
+        return employeeCode;
+    }
+
+    public void setEmployeeCode(String employeeCode) {
+        this.employeeCode = employeeCode;
+    }
+
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+
+
+
 
 }
