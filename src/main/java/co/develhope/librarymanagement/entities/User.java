aea2e36c43895742d6255c8fc07734b2fcf9972e
Marioy96 <marioscarselli1@gmail.com>
2022-08-23 12:53:05 +0200

    Ho aggiunto ID, il costruttore e ho fatto i metodi getters and setters

--- src/main/java/co/develhope/librarymanagement/entities/User.java
+++ src/main/java/co/develhope/librarymanagement/entities/User.java
@@ -10,5 +10,62 @@ public class User {
     private String name;
     private String surname;
     private String email;
+    private int id;
 
+    public User(String username, String password, String name, String surname, String email, int id) {
+        this.username = username;
+        this.password = password;
+        this.name = name;
+        this.surname = surname;
+        this.email = email;
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
 }
