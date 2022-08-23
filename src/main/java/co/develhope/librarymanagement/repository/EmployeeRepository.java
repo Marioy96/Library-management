365ee4fa79af5e27b17ca4dbad21b3026a3a6d7d
Marioy96 <marioscarselli1@gmail.com>
2022-08-23 12:45:50 +0200

    Ho aggiunto i metodi nella classe EmployeeService  (insert, delete and update)

--- src/main/java/co/develhope/librarymanagement/repository/EmployeeRepository.java
+++ src/main/java/co/develhope/librarymanagement/repository/EmployeeRepository.java
@@ -1,7 +1,15 @@
 package co.develhope.librarymanagement.repository;
 
+import co.develhope.librarymanagement.entities.Employee;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface EmployeeRepository {
+    List<Employee> findAll();
+
+    void save(Employee newEmployee);
+
+    void deleteAll();
 }
--- src/main/java/co/develhope/librarymanagement/service/EmployeeService.java
+++ src/main/java/co/develhope/librarymanagement/service/EmployeeService.java
@@ -1,8 +1,36 @@
 package co.develhope.librarymanagement.service;
 
+import co.develhope.librarymanagement.entities.Employee;
+import co.develhope.librarymanagement.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 // TODO implementare i metodi crud nel service,per gli esempi guarda la classe AuthorService
 @Service
 public class EmployeeService {
+
+    @Autowired
+    EmployeeRepository employeeRepository;
+
+    public List<Employee> getAllEmployee() {
+        return employeeRepository.findAll();
+    }
+
+    public void insertNewEmployee(Employee newEmployee) {
+        employeeRepository.save(newEmployee);
+    }
+
+    public void deleteAllEmployee(){employeeRepository.deleteAll();
+    }
+
+    public void updateEmployee(Employee employee){employeeRepository.save(new Employee());
+    }
+
+
+
+
+
+
 }
