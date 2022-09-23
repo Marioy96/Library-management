f45ca261b609e54c94e052538f387746d363bc41
LindaAmadei <108941806+LindaAmadei@users.noreply.github.com>
2022-09-23 12:57:58 +0200

    creazione fk book per warehouse

--- src/main/java/co/develhope/librarymanagement/entities/Warehouse.java
+++ src/main/java/co/develhope/librarymanagement/entities/Warehouse.java
@@ -16,11 +16,45 @@ public class Warehouse {
 
     @Column(name = "availability")
     private long availability;
+    private int fkBook;
 
-    @OneToMany(fetch = FetchType.LAZY,cascade = CascadeType.ALL)
-    @JoinColumn(name = "books_id")
-    private List<Book> loan = new ArrayList<>();
+    //@OneToMany(fetch = FetchType.LAZY,cascade = CascadeType.ALL)
+    //@JoinColumn(name = "books_id")
+    //private List<Book> loan = new ArrayList<>();
+
+
+    public Warehouse(){}
+       public Warehouse(int id, long availability, int fkBook) {
+        this.id = id;
+        this.availability = availability;
+        this.fkBook = fkBook;
+       }
+
+    public int getId() {
+        return id;
     }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public long getAvailability() {
+        return availability;
+    }
+
+    public void setAvailability(long availability) {
+        this.availability = availability;
+    }
+
+    public int getFkGioco() {
+        return fkBook;
+    }
+
+    public void setFkGioco(int fkGioco) {
+        this.fkBook = fkGioco;
+    }
+}
+
 
 
 
